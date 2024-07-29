4ba58f8005bd2665782e01398d831533bfa25b40
hemanthsuram87 <hemanthsuram87@gmail.com>
2024-07-29 16:29:05 -0400

    Additional changes with BDD test cases

--- src/main/java/com/restaurant/app/repository/RestaurantFoodItemRepository.java
+++ src/main/java/com/restaurant/app/repository/RestaurantFoodItemRepository.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
-@Repository
+
 public interface RestaurantFoodItemRepository extends MongoRepository<FoodItem,String> {
 List<FoodItem> findAll();
 
